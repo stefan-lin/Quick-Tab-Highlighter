9305601d0228bfeb5039b5a44008ba77cca2966d
stefan-lin <sir.stefee@gmail.com>
2016-07-19 19:31:46 -0700

    messaging partial done
    from popup to content messaging <done>
    from content to background <not done>
    canvas not working

--- js/app/content.js
+++ js/app/content.js
@@ -9,28 +9,44 @@
  */
 function update_favicon(color_code){
   var favicon_link_element = document.querySelector("head link[rel~='icon']");
-  if(!favicon_url) {
+  if(!favicon_link_element) {
     favicon_link_element = document.createElement("link");
     favicon_link_element.setAttribute("rel", "icon");
     document.body.appendChild(favicon_link_element);
   }
-  var new_favicon = localStorage[color_code];
+  
+  // TODO: fix this part later (isolate the localStorage)
+  delete localStorage[color_code];
+  //var new_favicon = localStorage[color_code];
+  var new_favicon = null;
+  
+  
   if(new_favicon) {
     favicon_link_element.type = "image/x-icon";
     favicon_link_element.href = new_favicon;
   }
   else{
-    chrome.extension.sendRequest(color_code, function(response) {
-      favicon_link_element.type = "image/x-icon";
-      favicon_link_element.href = response;
-      localStorage[color_code] = response;
+    console.log('[content] sending message from content to background');
+    chrome.runtime.sendMessage({
+        color  : color_code,
+        from   : 'content',
+        subject: 'HighlightTab'
+      },
+      function(new_favicon_url) {
+        favicon_link_element.type = "image/x-icon";
+        favicon_link_element.href = new_favicon_url;
+        localStorage[color_code]  = new_favicon_url;
+        console.log('favicon replaced and saved')
     });
   } // END IF-ELSE
 } // END FUNCTION
 
-chrome.runtime.onMessage.addListener(function(message, sender, response){
-  // VALIDATE THE MESSAGE RECEIVED
-  if(message.from === 'popup' && message.subject === 'HighlightTab'){
-    
-  }
+chrome.runtime.onMessage.addListener(
+  function(message, sender, callback){
+    console.log('content string received message');
+    // VALIDATE THE MESSAGE RECEIVED
+    if(message.from == 'popup' && message.subject == 'HighlightTab'){
+      update_favicon(message.color);
+      callback({msg: 'update_favicon_complete!'});
+    }
 });
--- js/app/popup.js
+++ js/app/popup.js
@@ -15,6 +15,10 @@ const ORG_ID  = '#orange-button';
 const OPT_URL = 'option.html';
 const HIS_URL = 'history.html';
 
+function print_response_to_log(response_message){
+  console.log(response_message);
+}
+
 function reg_option_listener(){
   $(OPT_ID).addEventListener('click', function(event){
     
@@ -22,10 +26,37 @@ function reg_option_listener(){
 }
 
 function reg_red_listener(){
-  $(RED_ID).addEventListener('click', function(event){
-    /*
-    SEND MESSAGE TO CONTENT SCRIPT
-     */
-    
+  $(RED_ID).on('click', function(){
+    console.log('red button clicked');
+    //var background_color = $(RED_ID).attr('background-color');
+    var background_color = '#FF3300'
+    console.log(background_color);
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { from   : 'popup', subject: 'HighlightTab', color  : background_color},
+        function(response){
+          console.log(response.msg);
+        }
+      );  // END chrome.tabs.sendMessage
+    }); // END chrome.tabs.query
+    //chrome.tabs.getSelected(null, function(tab){
+    //  console.log(tab.id);
+    //  chrome.tabs.sendMessage(
+    //    tab.id,
+    //    {from: 'popup', subject: 'HighlightTab', color: background_color},
+    //    function(response) {
+    //      console.log(response.msg);
+    //    }
+    //  );
+    //});
   });
 }
+
+
+/**
+ * INIT STEP
+ */
+document.addEventListener('DOMContentLoaded', function(){
+  reg_red_listener();
+});
