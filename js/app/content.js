f2c30535389433959d757b322d41744ebe5491bb
stefan-lin <sir.stefee@gmail.com>
2016-07-22 18:29:51 -0700

    keyboard shortcut and history

--- js/app/content.js
+++ js/app/content.js
@@ -3,16 +3,93 @@
  */
 
 // GLOBAL
-const BLOCK_GRAPHIC_CHAR = '&#9608';
+var origin_favicon_url   = null;
+var origin_title         = null;
+var favicon_link_element = null;
+var tab_url              = null;
 
 function title_marqee_effect(origin_title, special_char){
   document.title = origin_title + special_char;
   var c = origin_title.charAt(0);
   var s = origin_title.substring(1);
-  //s += c;
   var t = window.setTimeout(function(){
     title_marqee_effect(s, c);
   }, 300);
+}
+
+function collect_tabInfo(){
+  var link_ele = document.querySelector("head link[rel~='icon");
+  
+  if(!link_ele){
+    chrome.runtime.sendMessage({
+      from   : 'content',
+      subject: 'DefaultFavicon',
+      action:   'GET'
+    }, function(response){
+      console.log(response);
+      origin_favicon_url = response;
+    });
+  }
+  else{
+    origin_favicon_url = link_ele.href;
+  }
+  origin_title = document.title;
+}
+
+function create_link_element(){
+  console.log('[content] creating link element')
+  favicon_link_element = document.createElement("link");
+  favicon_link_element.setAttribute("rel", "shortcut icon");
+  document.head.appendChild(favicon_link_element);
+  console.log(favicon_link_element);
+}
+
+function shrink_img_to(online_url, width, height){
+  var image_template = new Image;
+  image_template.setAttribute('src', online_url);
+}
+
+// TODO: implement here to update the history
+function replace_favicon(new_url, color_code, special_char){
+  console.log(new_url);
+  console.log(origin_favicon_url);
+  var curr_url = favicon_link_element.href;
+  console.log(curr_url);
+  favicon_link_element.type = "image/x-icon";
+  if(new_url != curr_url){
+    favicon_link_element.href = new_url;
+    if(curr_url == origin_favicon_url){
+      document.title = special_char + document.title;
+    }
+    else{
+      document.title = special_char + document.title.substr(1);
+    }
+    // add entry to history
+    chrome.runtime.sendMessage({
+      color       : color_code,
+      special_char: special_char,
+      from        : 'content',
+      subject     : 'UpdateHistory',
+      action      : 'SET',
+      title       : origin_title
+    }, function(result){
+      console.log(result);
+    });
+  }
+  else{
+    favicon_link_element.href = origin_favicon_url;
+    document.title = origin_title;
+    // delete entry from history
+    chrome.runtime.sendMessage({
+      color: color_code,
+      special_char: special_char,
+      from: 'content',
+      subject: 'UpdateHistory',
+      action: 'DELETE'
+    }, function(result){
+      console.log(result);
+    });
+  }
 }
 
 /**
@@ -20,52 +97,51 @@ function title_marqee_effect(origin_title, special_char){
  * color_code is a string indicates color code
  */
 // TODO: maybe implement margee effect to the title
-function update_favicon(color_code){
-  var favicon_link_element = document.querySelector("head link[rel~='icon']");
-  
+function update_favicon(color_code, special_char){
+  favicon_link_element = document.querySelector("head link[rel~='icon']");
+  console.log(favicon_link_element);
   if(!favicon_link_element) {
-    favicon_link_element = document.createElement("link");
-    favicon_link_element.setAttribute("rel", "icon");
-    document.body.appendChild(favicon_link_element);
+    create_link_element();
   }
   
   // TODO: fix this part later (isolate the localStorage)
-  delete localStorage[color_code];
-  //var new_favicon = localStorage[color_code];
+  //delete localStorage[color_code];
   var new_favicon = null;
   
   
   if(new_favicon) {
-    favicon_link_element.type = "image/x-icon";
-    favicon_link_element.href = new_favicon;
+    replace_favicon(new_favicon, special_char);  ////TODO: FIX THIS ONE
   }
   else{
     console.log('[content] sending message from content to background');
     chrome.runtime.sendMessage({
-        color  : color_code,
-        from   : 'content',
-        subject: 'HighlightTab'
+        color   : color_code,
+        from    : 'content',
+        subject : 'HighlightTab',
+        curr_url: tab_url
       },
       function(new_favicon_url) {
         // REPLACE favicon
-        favicon_link_element.type = "image/x-icon";
-        favicon_link_element.href = new_favicon_url;
-        // STORE favicon FOR LATER USAGE
-        localStorage[color_code]  = new_favicon_url;
+        replace_favicon(new_favicon_url, color_code, special_char);
         
-        document.title = '\u26D4' + document.title;
-        //title_marqee_effect('Google', '\u26D4');
+        // STORE favicon FOR LATER USAGE
+        //localStorage[color_code]  = new_favicon_url;
         console.log('favicon replaced and saved')
     });
   } // END IF-ELSE
 } // END FUNCTION
 
+collect_tabInfo();
 chrome.runtime.onMessage.addListener(
   function(message, sender, callback){
     console.log('content string received message');
     // VALIDATE THE MESSAGE RECEIVED
     if(message.from == 'popup' && message.subject == 'HighlightTab'){
-      update_favicon(message.color);
+      update_favicon(message.color, message.special_char);
       callback({msg: 'update_favicon_complete!'});
+    }
+    if(message.from == 'background' && message.subject == 'HighlightTab'){
+      update_favicon(message.color_code, message.char_utf);
+      callback({msg: 'update_favicon complete for background'});
     }
 });
--- js/app/popup.js
+++ js/app/popup.js
@@ -7,11 +7,11 @@
  */
 const OPT_ID  = '#option-button';
 const HIS_ID  = '#history-button';
-const RED_ID  = '#red-button';
-const YLW_ID  = '#yellow-button';
-const GRN_ID  = '#green-button';
-const BLU_ID  = '#blue-button';
-const ORG_ID  = '#orange-button';
+const RED_ID  = '#button-1';
+const YLW_ID  = '#button-2';
+const GRN_ID  = '#button-3';
+const BLU_ID  = '#button-4';
+const ORG_ID  = '#button-5';
 const OPT_URL = 'option.html';
 const HIS_URL = 'history.html';
 
@@ -35,7 +35,12 @@ function reg_red_listener(){
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
       chrome.tabs.sendMessage(
         tabs[0].id,
-        { from   : 'popup', subject: 'HighlightTab', color  : background_color},
+        {
+          from        : 'popup',
+          subject     : 'HighlightTab',
+          color       : background_color,
+          special_char: '\u26D4'
+        },
         function(response){
           console.log(response.msg);
         }
@@ -44,10 +49,43 @@ function reg_red_listener(){
   });
 }
 
+function reg_listener(element_id, word_icon, color_code){
+  $(element_id + ':nth-child(1)').html(word_icon);
+  // NEED TO SET COLORS
+  $(element_id).on('click', function(){
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+          from        : 'popup',
+          subject     : 'HighlightTab',
+          color       : color_code,
+          special_char: word_icon
+        },
+        function(response){
+          console.log(response.msg);
+        }); // END chrome.tabs.sendMessage
+    }); // END chrome.tabs.query
+  }); // END on
+} // END reg_listener
+
+function init_color_buttons(){
+  // MESSAGE backgroun.js FOR DATA
+  chrome.runtime.sendMessage(
+    {from: 'popup', subject: 'ColorButtonsInfo'},
+    function(response){
+      for(var button_id in response.color_settings){
+        var settings = response.color_settings[button_id];
+        reg_listener(button_id, settings.char_id, settings.color_id);
+      } // END FOR LOOP
+    } // END callback FUNCTION
+  ); // END chrome.runtime.sendMessage
+} // END init_color_buttons FUNCTION
 
 /**
  * INIT STEP
  */
 document.addEventListener('DOMContentLoaded', function(){
-  reg_red_listener();
+  //reg_red_listener();
+  init_color_buttons();
 });
